a33788c1afee4d2c991274aecbf581dbde1b6f25
Jessica Stokes <hello@jessicastokes.net>
2017-01-24 10:02:18 -0800

    🔤 Use alphabetic hash for string replacements

--- src/string-replacement-cache.js
+++ src/string-replacement-cache.js
@@ -1,31 +1,27 @@
 import hash from 'sha.js';
 import anyBase from 'any-base';
 
-const hexToBase52 = ((alphabet) =>
-  anyBase(anyBase.HEX, alphabet + alphabet.toUpperCase())
-)('abcdefghijklmnopqrstuvwxyz');
+const alphabeticHash = ((alphabet) => {
+  // we use `hexToBase52` so generated identities are exclusively alphabetic
+  // as this causes them to fly under parsers' radars more effectively
+  const hexToBase52 = anyBase(anyBase.HEX, alphabet + alphabet.toUpperCase());
+  return (content) => hexToBase52(hash('sha256').update(content, 'utf-8').digest('hex'));
+})('abcdefghijklmnopqrstuvwxyz');
 
 const noOpReplacer = (thing) => thing;
 
 export default class StringReplacementCache {
-  constructor(expression, outputReplacer = noOpReplacer, identityReplacer = noOpReplacer, algorithm = 'sha256') {
+  constructor(expression, outputReplacer = noOpReplacer, identityReplacer = noOpReplacer) {
     this.expression = expression;
     this.outputReplacer = outputReplacer;
     this.identityReplacer = identityReplacer;
-    this.algorithm = algorithm;
     this._cache = {};
   }
 
   load(body) {
     const processed = body
       .replace(this.expression, (match, ...values) => {
-        // we use `hexToBase52` so generated identities are exclusively alphabetic
-        // as this causes them to fly under parsers' radars more effectively
-        const identityHash = hexToBase52(
-          hash(this.algorithm)
-            .update(match, 'utf-8')
-            .digest('hex')
-        );
+        const identityHash = alphabeticHash(match);
 
         const identity = this.identityReplacer(
             identityHash,
