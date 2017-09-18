7723b7c2fd3cd528877a67b64428e314b7533406
Jessica Stokes <hello@jessicastokes.net>
2017-09-18 12:36:57 -0700

    🚮 Remove react-dom/server dependency

--- app/index.js
+++ app/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactDOMServer from 'react-dom/server';
 import Homepage from './Homepage.mdx';
 
 import './index.css';
 
-if (typeof document !== 'undefined') {
-  ReactDOM.render(
-    <Homepage />,
-    document.getElementById('root')
-  );
-}
-
-export default function(locals, callback) {
-  const string = ReactDOMServer.renderToString(<Homepage />);
-  callback(null, string);
-}
+ReactDOM.render(
+  <Homepage />,
+  document.getElementById('root')
+);
