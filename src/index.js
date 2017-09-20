c23293317d11db5f1041a801915504d5859ecdcb
Jessica Stokes <hello@jessicastokes.net>
2017-09-20 11:58:13 -0700

    🚚 Move the ~hack zone~ into its own file

--- src/index.js
+++ src/index.js
@@ -1,45 +1,13 @@
 import frontMatter from 'front-matter';
 import { getLoaderConfig } from 'loader-utils';
 import HighlightJS from 'highlight.js';
+import MarkdownIt from './jsx-friendly-markdown-it';
 
 import formatImport from './formatters/import';
 import formatModule from './formatters/module';
 import formatStatic from './formatters/static';
 import StringReplacementCache from './string-replacement-cache';
 import walkHtml from './process-html';
-
-const MarkdownIt = (() => {
-  // welcome, to the
-  //
-  //  d8b                        d8b
-  //  ?88                        ?88
-  //   88b                        88b
-  //   888888b  d888b8b   d8888b  888  d88'
-  //   88P `?8bd8P' ?88  d8P' `P  888bd8P'
-  //  d88   88P88b  ,88b 88b     d88888b
-  // d88'   88b`?88P'`88b`?888P'd88' `?88b,
-  //
-  // d88888P  d8888b   88bd88b  d8888b
-  //    d8P' d8P' ?88  88P' ?8bd8b_,dP
-  //  d8P'   88b  d88 d88   88P88b
-  // d88888P'`?8888P'd88'   88b`?888P'
-  //
-  // Replace MarkdownIt's internal HTML regexes with JSX-friendly ones
-  const markdownItHtmlRegexes = require('markdown-it/lib/common/html_re');
-  Object.keys(markdownItHtmlRegexes).forEach((regexName) => {
-    const regex = markdownItHtmlRegexes[regexName];
-    // this adds support for the `.` character within tag names
-    markdownItHtmlRegexes[regexName] = new RegExp(
-      regex.source.replace(
-        /\[A\-Za\-z\]\[A\-Za\-z0\-9\\-\]\*/g,
-        '[A-Za-z][A-Za-z0-9\\.\\-]*'
-      ),
-      regex.flags
-    );
-  });
-
-  return require('markdown-it');
-})();
 
 // const ASSIGNMENT_COMMENT_PREFIX = '[mcl-assignment]:';
 
---
+++ src/jsx-friendly-markdown-it.js
@@ -0,0 +1,30 @@
+// welcome, to the
+//
+//  d8b                        d8b
+//  ?88                        ?88
+//   88b                        88b
+//   888888b  d888b8b   d8888b  888  d88'
+//   88P `?8bd8P' ?88  d8P' `P  888bd8P'
+//  d88   88P88b  ,88b 88b     d88888b
+// d88'   88b`?88P'`88b`?888P'd88' `?88b,
+//
+// d88888P  d8888b   88bd88b  d8888b
+//    d8P' d8P' ?88  88P' ?8bd8b_,dP
+//  d8P'   88b  d88 d88   88P88b
+// d88888P'`?8888P'd88'   88b`?888P'
+//
+// Replace MarkdownIt's internal HTML regexes with JSX-friendly ones
+const markdownItHtmlRegexes = require('markdown-it/lib/common/html_re');
+Object.keys(markdownItHtmlRegexes).forEach((regexName) => {
+  const regex = markdownItHtmlRegexes[regexName];
+  // this adds support for the `.` character within tag names
+  markdownItHtmlRegexes[regexName] = new RegExp(
+    regex.source.replace(
+      /\[A\-Za\-z\]\[A\-Za\-z0\-9\\-\]\*/g,
+      '[A-Za-z][A-Za-z0-9\\.\\-]*'
+    ),
+    regex.flags
+  );
+});
+
+export default require('markdown-it');
