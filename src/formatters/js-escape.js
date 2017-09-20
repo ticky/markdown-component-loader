86042c67662b21fb0ae995da0b60836b858f8d89
Jessica Stokes <hello@jessicastokes.net>
2017-09-19 19:14:25 -0700

    🎞 Put back jsesc stuff

--- src/formatters/js-escape.js
+++
@@ -1,9 +0,0 @@
-import JSEsc from 'jsesc';
-
-const JSESC_CONFIG = {
-  compact: false,
-  indent: '  ',
-  wrap: true
-};
-
-export default (javascript) => JSEsc(javascript, JSESC_CONFIG);
--- src/formatters/static.js
+++ src/formatters/static.js
@@ -1,3 +1,11 @@
-import doEscape from './js-escape';
+import JSEsc from 'jsesc';
+
+const JSESC_CONFIG = {
+  compact: false,
+  indent: '  ',
+  wrap: true
+};
+
+const doEscape = (javascript) => JSEsc(javascript, JSESC_CONFIG);
 
 export default (name, value) => `\nMarkdownComponent[${doEscape(name)}] = ${doEscape(value)};\n`;
